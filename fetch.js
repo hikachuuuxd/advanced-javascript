943f604fbda4a8457f8f8b3b9ffbb10ee8de0d70
nisrinasf <nisrinashofwahfirdaus@gmail.com>
2023-07-01 21:48:52 +0700

    eps 13 done

--- fetch.js
+++ fetch.js
@@ -67,29 +67,35 @@ $('#search-input').on('keyup', function(e)
 */
 
 const search = document.getElementById('search-button')
-search.addEventListener('click', function(){
-    let input = document.getElementById('search-input')
-    fetch('http://www.omdbapi.com/?apikey=5ba8bf5e&s=' + input.value)
-        .then(res => res.json())
-        .then(res => {
-            const movies = res.Search
-            let value = ''
-            movies.forEach(movie =>  value += `
-            
-            <div class="col">
-            <div class="card" style="width: 18rem;">
-            <img src="`+ movie.Poster +`" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${movie.Title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
-            
-            <a href="#" class="">See detail</a>
-            </div>
-            </div>
-            </div>
-            
-            `)
-        const card = document.getElementById('movie-list')
-        card.innerHTML = value
-        })
+search.addEventListener('click', async function(){
+    let input = document.getElementById('search-input').value
+    let movies = await getMovie(input)
+    showMovie(movies)
 })
+
+function getMovie(input){
+    
+    return fetch('http://www.omdbapi.com/?apikey=5ba8bf5e&s=' + input)
+        .then(res => res.json())
+        .then(res => res.Search)
+}
+
+function showMovie(getMovie){
+    let value = ''
+    getMovie.forEach(movie =>  value += show(movie) )
+    const card = document.getElementById('movie-list')
+     card.innerHTML = value
+}
+
+function show(movie){
+    return `<div class="col">
+    <div class="card" style="width: 18rem;">
+    <img src="`+ movie.Poster +`" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${movie.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+        <a href="#" class="">See detail</a>
+        </div>
+    </div>
+    </div>`
+}
